32d6351d510eb1d52afd06da248c6e6f8fbf47e3
bonbo0811 <abcdef999gp811@gmail.com>
2022-07-24 16:41:38 +0900

    ゲーム問題画面作成

--- app.js
+++ app.js
@@ -1,35 +1,72 @@
-const question = 'ゲーム史上最も売れたゲーム機は次の内どれ？';
-const answers = [
-        'スーパーファミコン',
-        'プレイステーション２',
-        'ファミリーコンピューター',
-        'ニンテンドーDS'
-    ];
-const correct = 'ニンテンドーDS';
+const quiz =[
+    {
+        question : 'ゲーム史上最も売れたゲーム機は次の内どれ？',
+        answers : [
+            'スーパーファミコン',
+            'プレイステーション２',
+            'ファミリーコンピューター',
+            'ニンテンドーDS'
+        ],
+        correct : 'ニンテンドーDS',
+    },{
+        question : 'ポケットモンスターを制作したゲーム会社は次の内どれ？',
+        answers : [
+            '任天堂',
+            'エニックス',
+            'ゲームフリーク',
+            'カプコン'
+        ],
+        correct : 'ゲームフリーク',
+    },{
+        question : 'ドラゴンクエスト５の隠しボスは次の内どれ？',
+        answers : [
+            'ゾーマ',
+            'デスタムーア',
+            'ダークドレアム',
+            'エスターク'
+        ],
+        correct : 'エスターク',
+    },
+];
+
+let quizIndex = 0;
+const quizLength = quiz.length;
+let score = 0;
 
 let buttonlength = document.getElementsByTagName('button').length;
 
 // クイズの問題文、選択肢を定義
 const setQuiz = () => {
-    document.getElementById('js-question-1').textContent = question;
+    document.getElementById('js-question-1').textContent = quiz[quizIndex].question;
     let buttonIndex = 0;
 
     while(buttonIndex < buttonlength){
         // ココに命令文
-        document.getElementsByTagName('button')[buttonIndex].textContent = answers[buttonIndex];
+        document.getElementsByTagName('button')[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
         buttonIndex++;
     }
-}
+};
 
 setQuiz();
 
 const clickHandler = (e) => {
-    if(correct === e.target.textContent) {
+    if(quiz[quizIndex].correct === e.target.textContent) {
         window.alert('正解！');
+        score++;
     } else {
         window.alert('不正解');
     }
-}
+
+    quizIndex++;
+
+    if(quizIndex < quizLength){
+        // 次の問題へ移行
+        setQuiz();
+    }else{
+        // クイズ終了
+        window.alert('クイズ終了。　あなたの正解数は' + score + '/' + quizLength + 'です！');
+    }
+};
 
 // ボタンがクリックされたら正誤判定
 let handleIndex = 0;
