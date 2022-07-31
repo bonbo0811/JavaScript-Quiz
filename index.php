fe50870fcda8191b44e25dd86caa1457a8943b5a
bonbo0811 <abcdef999gp811@gmail.com>
2022-07-31 21:59:53 +0900

    tab機能追加

--- index.php
+++ index.php
@@ -11,42 +11,59 @@
         <h6>いろいろなジャンルのクイズを集めてみました。<br>挑戦してみましょう！！</h6>
     </div>
 
-    <div class="quiz-list list-group col-md-7 mt-4 m-auto px-3">
-        <span class="list-group-item list-group-item-action active list-img bg-primary" aria-current="true">
-            クイズ一覧
-        </span>
-        <a href="quiz-fashions.php" class="list-group-item list-group-item-action">
-            <img src="images/fashion_image.jpeg" alt="アイコン" width="25" height="25" class="d-inline-block align-text-top ms-2">
-            ファッション
-        </a>
-        <a href="quiz-games.php" class="list-group-item list-group-item-action ">
-            <img src="images/game_image.jpeg" alt="アイコン" width="25" height="25" class="d-inline-block align-text-top ms-2">
-            ゲーム
-        </a>
-        <a href="quiz-movies.php" class="list-group-item list-group-item-action">
-            <img src="images/movie_image.jpeg" alt="アイコン" width="25" height="25" class="d-inline-block align-text-top ms-2">
-            映画
-        </a>
-        <a href="quiz-comics.php" class="list-group-item list-group-item-action">
-            <img src="images/comic_image.jpeg" alt="アイコン" width="25" height="25" class="d-inline-block align-text-top ms-2">
-            マンガ
-        </a>
-        <a href="quiz-foods.php" class="list-group-item list-group-item-action">
-            <img src="images/food_image.jpeg" alt="アイコン" width="25" height="25" class="d-inline-block align-text-top ms-2">
-            食べ物
-        </a>
-        <a href="quiz-kotowaza.php" class="list-group-item list-group-item-action">
-            <img src="images/kotowaza.jpeg" alt="アイコン" width="25" height="25" class="d-inline-block align-text-top ms-2">
-            ことわざ
-        </a>
-        <a href="quiz-englishwords.php" class="list-group-item list-group-item-action">
-            <img src="images/englishword.jpeg" alt="アイコン" width="25" height="25" class="d-inline-block align-text-top ms-2">
-            英単語
-        </a>
-        <a href="quiz-manners.php" class="list-group-item list-group-item-action">
-            <img src="images/manner.jpeg" alt="アイコン" width="25" height="25" class="d-inline-block align-text-top ms-2">
-            一般マナー
-        </a>
+    <div class="tab" id="js-tab">
+        <div class="tab-nav text-center">
+            <a href="" class="tab-nav-item is-active" data-nav="0">エンタメ</a>
+            <a href="" class="tab-nav-item" data-nav="1">勉強</a>
+            <a href="" class="tab-nav-item" data-nav="2">教養</a>
+        </div>
+
+        <div class="quiz-list list-group col-md-7 mt-4 m-auto px-3">
+            <span class="list-group-item list-group-item-action active list-img bg-primary" aria-current="true">
+                クイズ一覧
+            </span>
+            <div class="tab-contents-item" data-content="0">
+                <a href="quiz-fashions.php" class="list-group-item list-group-item-action">
+                    <img src="images/fashion_image.jpeg" alt="アイコン" width="25" height="25" class="d-inline-block align-text-top ms-2">
+                    ファッション
+                </a>
+                <a href="quiz-games.php" class="list-group-item list-group-item-action ">
+                    <img src="images/game_image.jpeg" alt="アイコン" width="25" height="25" class="d-inline-block align-text-top ms-2">
+                    ゲーム
+                </a>
+                <a href="quiz-movies.php" class="list-group-item list-group-item-action">
+                    <img src="images/movie_image.jpeg" alt="アイコン" width="25" height="25" class="d-inline-block align-text-top ms-2">
+                    映画
+                </a>
+                <a href="quiz-comics.php" class="list-group-item list-group-item-action">
+                    <img src="images/comic_image.jpeg" alt="アイコン" width="25" height="25" class="d-inline-block align-text-top ms-2">
+                    マンガ
+                </a>
+                <a href="quiz-foods.php" class="list-group-item list-group-item-action">
+                    <img src="images/food_image.jpeg" alt="アイコン" width="25" height="25" class="d-inline-block align-text-top ms-2">
+                    食べ物
+                </a>
+            </div>
+
+            <div class="tab-contents-item" data-content="1">
+                <a href="quiz-englishwords.php" class="list-group-item list-group-item-action">
+                    <img src="images/englishword.jpeg" alt="アイコン" width="25" height="25" class="d-inline-block align-text-top ms-2">
+                    英単語
+                </a>
+                <a href="quiz-kotowaza.php" class="list-group-item list-group-item-action">
+                    <img src="images/kotowaza.jpeg" alt="アイコン" width="25" height="25" class="d-inline-block align-text-top ms-2">
+                    ことわざ
+                </a>
+            </div>
+
+            <div class="tab-contents-item" data-content="2">
+                <a href="quiz-manners.php" class="list-group-item list-group-item-action">
+                    <img src="images/manner.jpeg" alt="アイコン" width="25" height="25" class="d-inline-block align-text-top ms-2">
+                    一般マナー
+                </a>
+            </div>
+        </div>
     </div>
+<script type="text/javascript" src="./JavaScript/tab.js"></script>
 </body>
 </html>
